941aa818ef8b3f4869de7064a2bbe62abe271f81
github username <github email address>
2019-07-29 11:05:53 -0700

    Association done

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ApplicationRecord
+    has_many :users, through: :dishes
+    has_many :recipes, through: :dishes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+    has_many :users, through: :dishes
+    has_many :ingredients, through: :dishes
 end
