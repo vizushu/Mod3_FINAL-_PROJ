40c5ace78f9c4aff3e583cae113eb16f5bf9491c
daniemart <github email address>
2019-07-29 14:29:56 -0700

    added js info

--- cook-api-backend/app/controllers/users_controller.rb
+++ cook-api-backend/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render JSON
+  end
+
+  def show
+    user = User.find(params[:id])
+    render JSON
+  end
 end
